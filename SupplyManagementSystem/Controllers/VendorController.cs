c830bac21b012b80dbe3d6412d592ba4b95a2d70
Febrianto752 <febrianto.bekasi@gmail.com>
2024-01-05 09:24:11 +0700

    feat: add vendor list page

---
+++ SupplyManagementSystem/Controllers/VendorController.cs
@@ -0,0 +1,27 @@
+﻿using SupplyManagementSystem.Data;
+using SupplyManagementSystem.Repositories;
+using SupplyManagementSystem.Repositories.IRepositories;
+using System.Web.Mvc;
+
+namespace SupplyManagementSystem.Controllers
+{
+    public class VendorController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IVendorRepository _vendorRepository;
+        private readonly ApplicationDbContext _db;
+
+        public VendorController()
+        {
+            _db = new ApplicationDbContext();
+            _accountRepository = new AccountRepository(_db);
+            _vendorRepository = new VendorRepository(_db);
+        }
+
+        public ActionResult Index()
+        {
+            var vendors = _vendorRepository.GetAll(includeProperties: "Account");
+            return View(vendors);
+        }
+    }
+}
---
+++ SupplyManagementSystem/Utilities/Handlers/GetNameHandler.cs
@@ -0,0 +1,15 @@
+﻿using SupplyManagementSystem.Utilities.Enums;
+
+namespace SupplyManagementSystem.Utilities.Handlers
+{
+    public static class GetNameHandler
+    {
+        public static string GetVendorStatusName(VendorStatusEnum vendorStatus)
+        {
+            if (vendorStatus == VendorStatusEnum.Pending) return "Meminta bergabung";
+            if (vendorStatus == VendorStatusEnum.Accepted) return "Bergabung";
+            if (vendorStatus == VendorStatusEnum.ApproveByAdmin) return "Disetujui Admin";
+            else return "Ditolak";
+        }
+    }
+}
