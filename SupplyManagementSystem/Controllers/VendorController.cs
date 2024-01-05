37532d3af064b5a42240597805af9c84e6c26a5c
Febrianto752 <febrianto.bekasi@gmail.com>
2024-01-05 10:39:59 +0700

    feat: add delete vendor feature

--- SupplyManagementSystem/Controllers/VendorController.cs
+++ SupplyManagementSystem/Controllers/VendorController.cs
@@ -2,6 +2,7 @@
 using SupplyManagementSystem.Repositories;
 using SupplyManagementSystem.Repositories.IRepositories;
 using System;
+using System.IO;
 using System.Web.Mvc;
 
 namespace SupplyManagementSystem.Controllers
@@ -37,6 +38,55 @@ namespace SupplyManagementSystem.Controllers
             }
 
             return View(vendor);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Guid guid)
+        {
+            var vendor = _vendorRepository.Get(v => v.Guid == guid);
+            var account = _accountRepository.Get(v => v.Guid == vendor.AccountGuid);
+
+            if (vendor == null || account == null)
+            {
+                TempData["Error"] = "Data tidak ditemukan";
+                return RedirectToAction("Index");
+            }
+
+            try
+            {
+                string uploadFolder = Server.MapPath("~/Static/Images");
+
+                // Gabungkan path lengkap file gambar
+                string imagePath = Path.Combine(uploadFolder, vendor.ProfileImage);
+
+                if (System.IO.File.Exists(imagePath))
+                {
+                    // Hapus file jika ada
+                    System.IO.File.Delete(imagePath);
+                }
+            }
+            catch
+            {
+
+            }
+
+            var deletedVendorIsSuccess = _vendorRepository.Delete(vendor);
+
+            if (deletedVendorIsSuccess)
+            {
+                var deletedVendorAccountIsSuccess = _accountRepository.Delete(account);
+
+                if (deletedVendorAccountIsSuccess)
+                {
+                    TempData["Success"] = "Data vendor berhasil dihapus";
+                    return RedirectToAction("Index");
+                }
+
+            }
+            TempData["Error"] = "Gagal menghapus data vendor";
+            return RedirectToAction("Index");
+
+
         }
     }
 }
